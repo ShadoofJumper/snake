d201c499a918608add09d4bde385338260d99c67
Ihor <ShadoofJumper@gmail.com>
2019-05-06 21:24:13 +0300

    Complete lesson-2

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,8 +10,19 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            Draw(1, 3, '*');
+
+            Draw(4, 5, '#');
+
             Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+
         }
     }
 }
